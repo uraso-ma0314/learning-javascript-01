2f8adb0959239f499acf73e56adfedc47fbee314
urasoma0314@gmail.com <urasoma0314@gmail.com>
2019-09-05 17:35:04 +0900

    夏休み 課題

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var weight; 
-weight = prompt('BMIの計測。体重(kg)を教えて下さい'); 
-var height; height = prompt('身長(m)を教えて下さい'); var bmi = weight / (height * height); 
+weight = prompt('BMIの計測。体重(kg)教えろ'); 
+var height; height = prompt('身長(m)を教えろ'); var bmi = weight / (height * height); 
 var message = 'BMIは' + bmi + 'です'; 
 alert(message); 
