5d13e70d6b10d5946c3e7c48253e4f2b426cc725
urasoma0314@gmail.com <urasoma0314@gmail.com>
2019-09-05 17:25:52 +0900

    7/5 課題

---
+++ index.js
@@ -0,0 +1,5 @@
+var weight; 
+weight = prompt('BMIの計測。体重(kg)を教えて下さい'); 
+var height; height = prompt('身長(m)を教えて下さい'); var bmi = weight / (height * height); 
+var message = 'BMIは' + bmi + 'です'; 
+alert(message); 
--- node-main.js
+++
@@ -1,2 +0,0 @@
-//window.alert("Hello, Node.js!!");
-console.log("Goodmorning, Node.js!!");
